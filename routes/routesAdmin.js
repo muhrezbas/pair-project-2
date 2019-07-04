91276e998dfe141771513b433d4308e73cf4bf53
imam madani <gondez@pop-os.localdomain>
2019-07-04 20:45:41 +0700

    add middleware

--- routes/routesAdmin.js
+++ routes/routesAdmin.js
@@ -3,13 +3,20 @@ const router = express.Router()
 
 const Controller = require('../controller/controllerAdmin.js')
 
-router.get('/login', Controller.loginGet)
-      .post('/login', Controller.loginPost)
-
 router.get('/register', Controller.registerGet)
       .post('/register', Controller.registerPost)
+router.get('/login', Controller.loginGet)
+      .post('/login', Controller.loginPost)
+router.use((req,res,next) => {
+    console.log('masuk sini??')
+    if (!req.session.admin) {
+        res.redirect('/admin/login')
+    }else{
+        next()
+    }
+})
 router.get('/dashboard',Controller.show)
-router.get('/dashboard/delete/:id', Controller.delete)
 router.get('/dashboard/add', Controller.addProGet)
       .post('/dashboard/add', Controller.addProPost)
+router.get('/dashboard/delete/:id', Controller.delete)
 module.exports = router
