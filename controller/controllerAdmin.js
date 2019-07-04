e1737bcae0847fdf85ca9e6987cbc24c350b3f64
muhrezbas <muhrezbasuki@gmail.com>
2019-07-04 21:17:56 +0700

    hampir

--- controller/controllerAdmin.js
+++ controller/controllerAdmin.js
@@ -1,44 +1,55 @@
-const {Admin, Pro, Peripheral} = require('../models/index')
+const { Admin, Pro, Peripheral, ProPeripheral } = require('../models/index')
 const bcrypt = require('bcrypt')
 class AdminController {
     static loginGet(req, res) {
-       res.render('login',{flash: req.flash()})
+        res.render('login', { flash: req.flash() })
     }
 
     static loginPost(req, res) {
-        Admin.findOne({
-            where: {
-                email: req.body.email
-            }
+        let pros
+        Pro.findAll({
+
         })
-        .then(user => {
-            if (!user) {
-                throw new Error('email not found!')
-            }else {
-                const comparePass = bcrypt.compareSync(req.body.password, user.password)
-                if (!comparePass) {
-                    throw new Error('email and password doesn\'t match')
-                }else {
-                    req.session.admin = {
-                        id: user.id
+            .then((pro) => {
+                pros = pro
+                return Admin.findOne({
+                    where: {
+                        email: req.body.email
                     }
-                    res.render('admin', {user})
+                })
+            })
+            .then(user => {
+                if (!user) {
+                    throw new Error('email not found!')
+                } else {
+                    const comparePass = bcrypt.compareSync(req.body.password, user.password)
+                    if (!comparePass) {
+                        throw new Error('email and password doesn\'t match')
+                    } else {
+                        req.session.admin = {
+                            id: user.id
+                        }
+
+                        res.render('admin', {
+                            user: user,
+                            data: pros
+                        })
+                    }
                 }
-            }
-        })
-        .catch(err => {
-            req.flash('error', err.message)
-            res.redirect('/admin/login')
-        })
+            })
+            .catch(err => {
+                req.flash('error', err.message)
+                res.redirect('/admin/login')
+            })
     }
 
     static registerGet(req, res) {
         let flash = req.flash()
-        res.render('register', {flash})
+        res.render('register', { flash })
     }
 
     static registerPost(req, res) {
-        
+
         Admin.create(req.body)
             .then(usr => {
                 req.flash('success', 'register success.')
@@ -53,36 +64,95 @@ class AdminController {
 
     static show(req, res) {
         Pro.findAll({
-            include:[Peripheral]
+            include: [Peripheral]
         })
-        .then(data => {
-            res.render('admin',{data})
-        })
-        .catch(err => {
-            res.send(err)
-        })
+            .then(data => {
+                res.render('admin', { data })
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
 
     static delete(req, res) {
         Pro.destroy({
-            where:{
-                id:req.params.id
+            where: {
+                id: req.params.id
             }
         })
-        .then(num => {
-            res.redirect('/admin/dashboard')
-        })
-        .catch(err => {
-            res.send(err)
-        })
+            .then(num => {
+                res.redirect('/admin/dashboard')
+            })
+            .catch(err => {
+                res.send(err)
+            })
     }
 
     static addProGet(req, res) {
-        res.render('add-pro')
+        Peripheral.findAll()
+            .then((data) => {
+                res.render('add-pro', {
+                    data: data
+                })
+            })
+
+    }
+    static addPeripheralGet(req, res) {
+        res.render('add-per')
+
+    }
+    static addPeripheralPost(req, res) {
+        Peripheral.create(req.body)
+            .then(() => {
+                res.redirect('/admin/dashboard')
+            })
+
     }
 
     static addProPost(req, res) {
-        console.log(req.body)
+        let proId
+        let created = {
+            "name": req.body.name,
+            "born": new Date(req.body.born),
+            "team": req.body.team,
+            "mousepad": req.body.mousepad,
+            "dpi": req.body.dpi,
+            "edpi": req.body.edpi,
+            "sensitivity": req.body.sensitivity,
+            "hz": req.body.hz,
+            "zoom_sens": req.body.zoom_sense,
+            "mouse_accel": req.body.mouse_accel,
+            "raw_input": req.body.raw_input,
+            "crosshair": req.body.crosshair,
+            "role": req.body.role,
+            "view_model": req.body.view_model
+        }
+        Pro.create(created)
+            .then((data) => {
+                proId = data.id
+                return ProPeripheral.create({ ProId: proId, PeripheralId: req.body.mouse })
+            })
+            .then(() => {
+
+                return ProPeripheral.create({ ProId: proId, PeripheralId: req.body.mousepad })
+            })
+            .then(() => {
+
+                return ProPeripheral.create({ ProId: proId, PeripheralId: req.body.keyboard })
+            })
+            .then(() => {
+
+                return ProPeripheral.create({ ProId: proId, PeripheralId: req.body.gpu })
+            })
+            .then(() => {
+
+                return ProPeripheral.create({ ProId: proId, PeripheralId: req.body.headset })
+            })
+            .then(() => {
+                res.redirect('/admin/dashboard')
+            })
+
+        // 
     }
 }
 
--- routes/routesAdmin.js
+++ routes/routesAdmin.js
@@ -4,12 +4,14 @@ const router = express.Router()
 const Controller = require('../controller/controllerAdmin.js')
 
 router.get('/login', Controller.loginGet)
-      .post('/login', Controller.loginPost)
+router.post('/login', Controller.loginPost)
 
 router.get('/register', Controller.registerGet)
-      .post('/register', Controller.registerPost)
+router.post('/register', Controller.registerPost)
 router.get('/dashboard',Controller.show)
 router.get('/dashboard/delete/:id', Controller.delete)
 router.get('/dashboard/add', Controller.addProGet)
-      .post('/dashboard/add', Controller.addProPost)
+router.post('/dashboard/add', Controller.addProPost)
+router.get('/dashboard/add-peripheral', Controller.addPeripheralGet)
+router.post('/dashboard/add-peripheral', Controller.addPeripheralPost)
 module.exports = router
